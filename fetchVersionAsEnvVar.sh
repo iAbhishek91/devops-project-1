9ad40af276ac158abc864bf783bdb23dcfadfd9c
Abhishek Das <abhi.das2007das@gmail.com>
2020-10-12 22:56:57 +0100

    cd

--- fetchVersionAsEnvVar.sh
+++ fetchVersionAsEnvVar.sh
@@ -2,6 +2,6 @@
 
 
 # fetch the version from package.json
-export VERSION=$(cat package.json | grep version | awk -F: '{ print $2 }' | sed 's/[",]//g')
+export VERSION=$(cat package.json | grep version | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
-echo "::set-env name=VERSION::$VERSION"
+echo "VERSION=${VERSION}"
